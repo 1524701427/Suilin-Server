a14f811b19ee0a07076995b0d50f19ad9a22b6d6
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:42:13 +0800

    feat: add device mapper

---
+++ src/main/java/com/suilin/device/mapper/DeviceMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.device.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.device.entity.Device;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DeviceMapper extends BaseMapper<Device> {}
