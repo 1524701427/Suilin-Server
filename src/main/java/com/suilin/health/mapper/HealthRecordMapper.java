850743f50c82cb9b3a5a166af37555e031fb6eba
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:44:38 +0800

    feat: add health record mapper

---
+++ src/main/java/com/suilin/health/mapper/HealthRecordMapper.java
@@ -0,0 +1,4 @@
+package com.suilin.health.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.health.entity.HealthRecord;
+public interface HealthRecordMapper extends BaseMapper<HealthRecord> {}
