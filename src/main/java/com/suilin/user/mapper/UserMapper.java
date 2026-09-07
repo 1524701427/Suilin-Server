e69a4fa6da16242a89e0f8678a462a0dfb01a573
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:46:43 +0800

    feat: add user mapper

---
+++ src/main/java/com/suilin/user/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.suilin.user.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.user.entity.User;
+public interface UserMapper extends BaseMapper<User> {}
