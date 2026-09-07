be06fd2d1e81b186098b787e52a1ec770f04a2e6
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:43:48 +0800

    feat: add elder mapper

---
+++ src/main/java/com/suilin/elder/mapper/ElderMapper.java
@@ -0,0 +1,4 @@
+package com.suilin.elder.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.elder.entity.Elder;
+public interface ElderMapper extends BaseMapper<Elder> {}
