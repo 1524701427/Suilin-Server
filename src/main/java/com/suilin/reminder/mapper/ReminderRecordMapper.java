807d9b696a42e941c70ded7be29176c61a363f84
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:45:39 +0800

    feat: add reminder record mapper

---
+++ src/main/java/com/suilin/reminder/mapper/ReminderRecordMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.reminder.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.reminder.entity.ReminderRecord;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ReminderRecordMapper extends BaseMapper<ReminderRecord> {}
