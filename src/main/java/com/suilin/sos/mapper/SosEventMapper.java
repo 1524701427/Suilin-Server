e50ec6f6c3aa5c27acd050c2c4ae29cf8473664c
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:46:03 +0800

    feat: add SOS event mapper

---
+++ src/main/java/com/suilin/sos/mapper/SosEventMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.sos.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.sos.entity.SosEvent;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SosEventMapper extends BaseMapper<SosEvent> {}
