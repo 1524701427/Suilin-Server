fbbd184cbea6f2e77958c20d002af00865a5489d
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-07 17:43:59 +0800

    feat: add elder invitation mapper

---
+++ src/main/java/com/suilin/elder/mapper/ElderInviteMapper.java
@@ -0,0 +1,4 @@
+package com.suilin.elder.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.elder.entity.ElderInvite;
+public interface ElderInviteMapper extends BaseMapper<ElderInvite> {}
