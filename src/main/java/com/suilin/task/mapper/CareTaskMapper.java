247bf28d2fef29895bec25f196caf00d99427465
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-10 15:38:31 +0800

    feat: add care task mapper

---
+++ src/main/java/com/suilin/task/mapper/CareTaskMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.task.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.task.entity.CareTask;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CareTaskMapper extends BaseMapper<CareTask> {}
