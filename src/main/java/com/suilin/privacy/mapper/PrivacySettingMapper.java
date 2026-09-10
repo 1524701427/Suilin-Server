47dcf90f9f64e9055ee658a884569780c2c5bc4d
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-10 15:51:47 +0800

    feat: add privacy settings mapper

---
+++ src/main/java/com/suilin/privacy/mapper/PrivacySettingMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.privacy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.privacy.entity.PrivacySetting;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PrivacySettingMapper extends BaseMapper<PrivacySetting> {}
