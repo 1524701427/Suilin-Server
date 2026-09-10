d4ea2a3121a9ab67e529500045bd98a46cfdd52e
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-10 15:39:09 +0800

    feat: add notification settings mapper

---
+++ src/main/java/com/suilin/notification/mapper/NotificationSettingMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.notification.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.notification.entity.NotificationSetting;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface NotificationSettingMapper extends BaseMapper<NotificationSetting> {}
