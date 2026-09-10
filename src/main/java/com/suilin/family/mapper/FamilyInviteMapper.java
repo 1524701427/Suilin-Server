64f1043e4fba1645583ec570c086b1acaf685b16
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-10 15:35:37 +0800

    feat: add family invite mapper

---
+++ src/main/java/com/suilin/family/mapper/FamilyInviteMapper.java
@@ -0,0 +1,8 @@
+package com.suilin.family.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.suilin.family.entity.FamilyInvite;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FamilyInviteMapper extends BaseMapper<FamilyInvite> {}
